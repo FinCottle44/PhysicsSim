193d1eaae8cb0a171de9034e9a0f1979b5cb1afc
FinCottle44 <37659422+FinCottle44@users.noreply.github.com>
2018-05-02 12:57:48 +0100

    02/05/18 Part 2
    Added grid system! Fundementatl to development of realtime construction idea.
    Along with 2d view to make construction process easier. (it will be symmetrical either side.)

--- Assets/_Scripts/BridgeDesign.cs
+++ Assets/_Scripts/BridgeDesign.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class BridgeDesign : MonoBehaviour {
     public GameObject particle;
+    public Texture crosshair;
+    public Camera cam;
+    public Vector2 screenPos;
+
+    bool click;
 
     // Use this for initialization
     void Start () {
-
-	}
+        cam = GetComponent<Camera>();
+        click = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		Draw ();
+		//Draw ();
 	}
 
-	void Draw(){
-		if (Input.GetMouseButtonDown(0)) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray))
-                Instantiate(particle, transform.position, transform.rotation);
-        }
-	}
+	//void Draw(){
+	//	if (Input.GetMouseButton(0)) {
+ //           Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+ //           if (Physics.Raycast(ray))
+ //               click = true;
+ //               screenPos = Input.mousePosition;
+ //               Cursor.visible = false;
+ //       }
+ //       else if (Input.GetMouseButton(0) == false)
+ //       {
+ //           click = false;
+ //       }
+	//}
+
+ //   void OnGUI()
+ //   {
+        
+ //       if (click)
+ //       {
+ //           //GUI.DrawTexture(new Rect(Input.mousePosition, new Vector2(100, 100)), crosshair);
+ //           GUI.DrawTexture(new Rect(Input.mousePosition.x - 25, Screen.height - Input.mousePosition.y - 25, 50, 50), crosshair);
+ //           //click = false;
+ //       }
+ //   }
 }
