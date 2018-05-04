c0e627b81e401bec8d1b5a2a285207fdf4570cce
FinCottle44 <37659422+FinCottle44@users.noreply.github.com>
2018-05-04 12:41:22 +0100

    04/05/18 part 1
     - added rotation with trigonometry for bridge building

--- Assets/_Scripts/CubePlacer.cs
+++ Assets/_Scripts/CubePlacer.cs
@@ -8,6 +8,7 @@ public class CubePlacer : MonoBehaviour
 
     private Grid grid;
     private GameObject midCube;
+    private GameObject firstCube;
 
     private void Awake()
     {
@@ -39,26 +40,59 @@ public class CubePlacer : MonoBehaviour
         if (clickNum == 1)
         {
             startPos = finalPosition;
-            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+            //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+            firstCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            firstCube.transform.position = new Vector3(-3, finalPosition.y, finalPosition.z);
             clickNum = 2;
         }
         else if (clickNum == 2)
         {
             endPos = finalPosition;
-            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+            //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+            firstCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            firstCube.transform.position = new Vector3(-3, finalPosition.y, finalPosition.z);
+
             Vector3 mid = startPos + (endPos - startPos) / 2;
             midCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            midCube.transform.position = mid;
-            //midCube.transform.localScale = endPos - startPos;
+            midCube.transform.position = new Vector3(-3, mid.y, mid.z);
+            float changeY = Mathf.Abs(endPos.y - startPos.y);
+            float changeZ = Mathf.Abs(endPos.z - startPos.z);
+            float difYZ = changeY / changeZ;
+            float angle = Mathf.Atan(difYZ);
+            double result = RadianToDegree(angle);
+
+            Debug.Log("y: " + changeY + " z: " + changeZ);
+            Debug.Log(difYZ);
+            Debug.Log(angle);
+            Debug.Log((float)result);
+
             float xScale = Mathf.Abs(endPos.x - startPos.x);
+            float yScale = Mathf.Abs(endPos.y - startPos.y);
+            float zScale = Mathf.Abs(endPos.z - startPos.z);
 
             if (xScale == 0)
             {
                 xScale = 1;
             }
-            midCube.transform.localScale = new Vector3(xScale, Mathf.Abs(endPos.y - startPos.y), Mathf.Abs(endPos.z - startPos.z));
-            clickNum = 1;
-        }
+            if (yScale == 0)
+            {
+                yScale = 1;
+            }
+            if (zScale == 0)
+            {
+                zScale = 1;
+            }
+            midCube.transform.localScale = new Vector3(xScale, yScale, zScale);
+            midCube.transform.Rotate(new Vector3((float)result, 0, 0));
+            Debug.Log(midCube.transform.rotation);
 
+            clickNum = 1;
+
+        }
+    }
+
+    double RadianToDegree(double angle)
+    {
+        return angle * (180.0 / Mathf.PI);
     }
 }
--- Assets/_Scripts/camInit.cs
+++ Assets/_Scripts/camInit.cs
@@ -61,7 +61,6 @@ public class camInit : MonoBehaviour
         float minFov = 15f;
         float maxFov = 90f;
         float sensitivity = -10f; //negative sens inverts direction of scroll
-
         float fov = Camera.main.fieldOfView;
         fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
 		fov = Mathf.Clamp(fov, minFov, maxFov);
